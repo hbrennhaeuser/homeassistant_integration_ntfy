0a87b6c587c2a3a66aa344f7a14bdc48b89bf8b5
HBrennhaeuser <h.brennhaeuser@gmx.de>
2024-03-26 18:47:09 +0100

    Implement token-authentication and topic-override for each notifiation-call

--- custom_components/ntfy/notify.py
+++ custom_components/ntfy/notify.py
@@ -1,6 +1,5 @@
 """Ntfy notification service."""
-
-from tokenize import String
+import logging
 from homeassistant.components.notify import (
     ATTR_MESSAGE,
     ATTR_TITLE,
@@ -9,15 +8,19 @@ from homeassistant.components.notify import (
     PLATFORM_SCHEMA,
     BaseNotificationService,
 )
-
 import homeassistant.helpers.config_validation as cv
 
+from tokenize import String
 from requests.auth import HTTPBasicAuth
 import requests
-import logging
 import voluptuous as vol
+import urllib.parse
+from base64 import b64encode
 
-CONF_TOPIC="topic"
+
+CONF_TOPIC = 'topic'
+CONF_ALLOW_TOPIC_OVERRIDE = 'allow_topic_override'
+CONF_TOKEN = 'token'
 
 from homeassistant.const import (
     CONF_PASSWORD,
@@ -31,7 +34,7 @@ from homeassistant.const import (
 _LOGGER = logging.getLogger(__name__)
 
 
-def get_service(hass,config,discovery_info=None):
+def get_service(hass,config, discovery_info=None):
     return NtfyNotificationService(config)
 
 
@@ -39,60 +42,105 @@ def get_service(hass,config,discovery_info=None):
 class NtfyNotificationService(BaseNotificationService):
     def __init__ (self, config):
         """Initialize the Ntfy notification service."""
-        self.user = config.get(CONF_USERNAME)
-        _LOGGER.debug("Got User: ",self.user)
-        self.password = config.get(CONF_PASSWORD)
-        _LOGGER.debug("Got Password: XXXXXXXXXX")
+        self.request_timeout=5
 
-        self.topic = config.get(CONF_TOPIC)
-        _LOGGER.debug("Got User: ",self.user)
+        self.topic = None
+        if config.get(CONF_TOPIC) is not None:
+            self.topic = config.get(CONF_TOPIC)
 
         self.url = config.get(CONF_URL)
-        _LOGGER.debug("Got URL: ",self.url)
-        
-        self.auth = config.get(CONF_AUTHENTICATION)
-        _LOGGER.debug("Got Authentication: ",self.auth)
-        
-        self.verifyssl = config.get(CONF_VERIFY_SSL)
-        _LOGGER.debug("Got Verify_SSL: ",self.verifyssl)
+        try:
+            vol.Schema(vol.Url())(self.url)
+        except vol.MultipleInvalid as e:
+            raise SyntaxError('url syntax invalid') from e
+
+        self.verifyssl = True
+        if config.get(CONF_VERIFY_SSL) is not None:
+            self.verifyssl = bool(config.get(CONF_VERIFY_SSL))
+
+        self.allow_topic_override = False
+        if config.get(CONF_ALLOW_TOPIC_OVERRIDE) is not None:
+            self.allow_topic_override = bool(config.get(CONF_ALLOW_TOPIC_OVERRIDE))
+
+        self.auth = False
+        if config.get(CONF_AUTHENTICATION) is not None:
+            schema_authentication = ['user-pass','token']
+            if config.get(CONF_AUTHENTICATION) not in schema_authentication:
+                raise SyntaxError('Invalid value specified for authentication')
+            self.auth = config.get(CONF_AUTHENTICATION)
+
+        self.username = None
+        self.password = None
+        self.token = None
+        if self.auth is not False:
+            if self.auth == 'user-pass':
+                if config.get(CONF_USERNAME) is None:
+                    raise SyntaxError("Authentication username is missing")
+                if config.get(CONF_PASSWORD) is None:
+                    raise SyntaxError("Authentication password is missing")
+                self.username = config.get(CONF_USERNAME)
+                self.password = config.get(CONF_PASSWORD)
+            elif self.auth == 'token':
+                if config.get(CONF_TOKEN) is None:
+                    raise SyntaxError('Authentication token is missing')
+                self.token = config.get(CONF_TOKEN)
 
 
 
     def send_message(self, message="", **kwargs):
-        
+        """Send message"""
         title=kwargs.get(ATTR_TITLE,ATTR_TITLE_DEFAULT)
         data=kwargs.get(ATTR_DATA,[])
+        topic=self.topic
 
-        
+        req_data=None
+        req_headers={}
+
+        # --
+        if "topic" in data:
+            if self.allow_topic_override:
+                topic=data["topic"]
+            else:
+                raise PermissionError('Trying to override topic without allow_topic_override being True')
+        if topic is None:
+            raise SyntaxError("No topic specified")
+        url=str(self.url) + '/' + urllib.parse.quote(str(topic))
+
+        # --
         req_data=message.encode('utf-8')
-        req_headers={
-            "Title": title.encode('utf-8')
-        }
-        
-        if data is not None:
-            if "tags" in data:
-                req_headers["Tags"] = data["tags"]
 
-            if "priority" in data:
-                schema_click = ['max','urgent','high','default','low','min']
-                if str(data["priority"]) not in schema_click:
-                    raise SyntaxError('Incorrect value for attribute priority given')
-                req_headers["Priority"] = data["priority"]
-    
-            if "click" in data:
-                schema_url = vol.Schema(vol.Url())
-                try:
-                    schema_url(data["click"])
-                except vol.MultipleInvalid as e:
-                    raise SyntaxError('expected a URL for attribute click')
-                req_headers["Click"] = data["click"]
-        
-        
-        req_verify=True
-        if self.verifyssl == False:
-            req_verify=False
+        if title is not None:
+            req_headers["Title"] = title
 
-        if self.auth == True :
-            requests.post(str(self.url)+'/'+str(self.topic),data=req_data,headers=req_headers,verify=req_verify,auth=HTTPBasicAuth(self.user,self.password))
-        else:
-            requests.post(str(self.url)+'/'+str(self.topic),data=req_data,headers=req_headers,verify=req_verify)
+        if "tags" in data:
+            # TODO: validate tag format
+            req_headers["Tags"] = data["tags"]
+
+        if "priority" in data:
+            schema_priority = ['max','urgent','high','default','low','min']
+            if str(data["priority"]) not in schema_priority:
+                raise SyntaxError('Incorrect value for attribute priority given')
+            req_headers["Priority"] = data["priority"]
+
+        if "click" in data:
+            schema_url = vol.Schema(vol.Url())
+            try:
+                schema_url(data["click"])
+            except vol.MultipleInvalid as e:
+                raise SyntaxError('expected a URL for attribute click') from e
+            req_headers["Click"] = data["click"]
+
+
+        # --
+        if self.auth == 'user-pass':
+            req_headers['Authorization'] = 'Basic ' + b64encode( f"{self.username}:{self.password}".encode() ).decode()
+        elif self.auth == 'token':
+            req_headers['Authorization'] = 'Bearer ' + self.token
+
+        requests.post(
+            url=url,
+            data=req_data,
+            headers=req_headers,
+            verify=self.verifyssl,
+            timeout=self.request_timeout
+        )
--- readme.md
+++ readme.md
@@ -1,9 +1,8 @@
 # Notify via ntfy.sh / selfhosted ntfy-server
-[![hacs_badge](https://img.shields.io/badge/HACS-Custom-41BDF5.svg)](https://github.com/hacs/integration) ![hacs_validation](https://github.com/hbrennhaeuser/homeassistant_integration_ntfy/actions/workflows/hacs_validation.yml/badge.svg?branch=main) ![validate_with_hassfest](https://github.com/hbrennhaeuser/homeassistant_integration_ntfy/actions/workflows/validate_with_hassfest.yml/badge.svg?branch=main)
-
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-41BDF5.svg)](https://github.com/hacs/integration)
 
 This custom component allows you to send notifications through [ntfy.sh](https://ntfy.sh/) or selfhosted ntfy-servers.
-Authentication and some additional ntfy-features like tags are supported.
+Authentication and additional ntfy-features like tags are supported.
 
 ## Installation
 
@@ -11,7 +10,7 @@ The recommended way to install this integration is through HACS.
 ### HACS
 
 Add this repository as a custom repository in hacs (category: integration).
-When the custom repository is added you can search for and install this integration like any other hacs-integration.
+When the custom repository is added you can search for and install this integration.
 
 Make sure to restart Homeassistant after the installation.
 
@@ -21,47 +20,65 @@ Copy custom_components/ntfy to config/custom_components/ntfy.
 
 Make sure to restart Homeassistant after the installation.
 
-
-
 ## Configuration
 
 Define a new ntfy notification-service in configuration.yaml:
 
+Example:
+
 ```yaml
 notify:
-    - name: ntfy_test
+    - name: ntfy_notification
       platform: ntfy
-      username: 'user'
-      password: 'password'
-      topic: 'test'
-      url: 'https://ntfy.domain.tld'
-      authentication: True
-      verify_ssl: True
+      authentication: 'token'
+      #username: 'user' 
+      #password: 'password' 
+      token: 'tk_odlbse211n74kf8N7h4qhqvj409qb'
+      topic: 'mytopic'
+      url: 'https://ntfy.domain.tld' 
+      #verify_ssl: True 
+      allow_topic_override: True 
 ```
 
-Set `authentication` to False to connect to the server anonymously.
+Options:
+
+| Option | Required | Default value | Values | Description |
+| --- | --- | --- | --- | --- |
+|authentication|No|False|user-pass/token/False|Specify authentication-type to use. Set to False to connect to the server anonymously.|
+|username|If authentication is 'user-pass'||username|ntfy username|
+|password|If authentication is 'user-pass'||password|ntfy password|
+|token|If authentication is 'token'||token|ntfy authentication token|
+|topic|No||topic|Topic to publish to. It's recommended to set the topic here, but you can also set it in each notification-call if allow_topic_override is True.|
+|url|Yes||url|ntfy-instance-url, example: https://ntfy.domain.tld|
+|verify_ssl|No|True|True/False|Specifies if the certificate of the ntfy-server should be verified. Set to False for self-signed certificates.|
+|allow_topic_override|No|False|True/False|Allow topic-override in each notification-call.|
 
 ## Usage
 
 Call the notification service anywhere in Homeassistant:
 
+Minimal call:
+
 ```yaml
-service: notify.ntfy_test
+service: notify.ntfy_notification
 data:
-  title: Homeassistant Notification
   message: Terrace door is open
 ```
 
-Optionally define additional data:
+Optional parameters/Additional data:
 
 ```yaml
-service: notify.ntfy_test
+service: notify.ntfy_notification
 data:
   title: Homeassistant Notification
   message: Terrace door is open
   data:
     tags: door
     priority: high
+    click: https://myhomassistant.domain.tld
+    topic: myothertopic
 ```
 
-Currently tags, priority and click are supported.
+Currently the ntfy-features tags, priority and click are supported. Please refer to the [ntfy.sh documentation](https://docs.ntfy.sh/publish) for more information about those features.
+
+You can override the default topic by providing a topic in the notification data.
